e0eb182da4870e0ec406e51477b0cc84bcd66d00
IliTheButterfly <iliana.decarufel@gmail.com>
2026-03-17 09:12:15 -0400

    Fixing latch Untested

--- src/joy_to_bool_node.cpp
+++ src/joy_to_bool_node.cpp
@@ -12,9 +12,7 @@ public:
     button1_index_ = this->declare_parameter<int>("button1_index", 0);
     button2_index_ = this->declare_parameter<int>("button2_index", -1);
 
-    if (button2_index_ != -1) {
-      latch_mode_ = true;
-    }
+    latch_mode_ = (button2_index_ != -1);
 
     // Publisher
     pub_ = this->create_publisher<std_msgs::msg::Bool>(flippers_topic_, 10);
@@ -60,14 +58,14 @@ private:
     }
     else
     {
-      if (button1_index_ < 0 || button1_index_ >= static_cast<int>(msg->buttons.size()))
+      if (button2_index_ < 0 || button2_index_ >= static_cast<int>(msg->buttons.size()))
       {
         RCLCPP_WARN_THROTTLE(
           get_logger(),
           *get_clock(),
           2000,
           "Button index %d out of range (buttons size: %zu)",
-          button1_index_,
+          button2_index_,
           msg->buttons.size()
         );
         latched_ = false;
@@ -107,7 +105,7 @@ private:
 
   int button1_index_;
   int button2_index_;
-  bool latch_mode_;
+  bool latch_mode_ = false;
   std::string joy_topic_ = "~/joy";
   std::string flippers_topic_ = "~/bool";
 
